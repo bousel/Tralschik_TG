cbf1380930b009f63b6c53557df72c5f525dc67c
L-003-MD <vlalokoz@outlook.com>
2023-04-20 22:49:12 +0300

    Пока ещё ноль. Добавил обработчики команд start, help, play, stop, а так же создал флажочек fl_play, который свидетельствует о начатой игре. В последствии он поможет либо принимать координаты, либо быть готовым к любому другому сообщению

---
+++ interface.py
@@ -0,0 +1,54 @@
+speech = [
+"""
+    Это простой бот для игры в сапера.
+    Используйте:
+    /help - для помощи
+    /play - для игры
+    """,
+    """
+    Тут будет помощь. Потом.
+    """,
+    """
+    Сыграем?
+    """,
+    """
+    Прекратили играть
+    """
+]
+
+
+import telebot
+
+token = open("token.txt").read()
+
+bot = telebot.TeleBot(token)
+fl_play:bool = False
+
+@bot.message_handler(commands=["start", "начало", "начинай", "стартуй"])
+def start(message):
+    bot.send_message(message.chat.id, speech[0])
+
+@bot.message_handler(commands=["help", "помоги"])
+def help(message):
+    bot.send_message(message.chat.id, speech[1])
+
+@bot.message_handler(commands=["play"])
+def play(message):
+    global fl_play
+    fl_play = True
+    bot.send_message(message.chat.id, speech[2])
+
+@bot.message_handler(commands=["stop"])
+def stop(message):
+    global fl_play
+    fl_play = False
+
+@bot.message_handler(func=lambda message: True)
+def msger(message):
+    if fl_play:
+        bot.send_message(message.chat.id, "Игра сейчас начата")
+    else:
+        bot.send_message(message.chat.id, "Игра сейчас не начата")
+
+
+bot.polling()

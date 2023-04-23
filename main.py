abbc08ce9d59052c26c9863eb451a0df0e2f2467
unknown <vlalokoz@yandex.ru>
2023-04-23 12:22:15 +0300

    Добавил генерацию игровой доски размера n*n, с m минами, расставленных в случайном порядке

--- main.py
+++ main.py
@@ -3,18 +3,30 @@ from random import sample as rt
 class Cell:
 
     def __init__(self, is_mine:bool=False, around_mines:int=0) -> None:
+        """
+        Создание объекта ячейки
+        """
         self.is_mine = is_mine
         self.around_mines = around_mines
         self.is_opened:bool = False
 
 
 class GamePole:
-
-    def __init__(self, n, m) -> None:
+    def __init__(self, n=10, m=6):
         """
         Создает игровое поле размером n*n с m минами
         int n - размерность игрового поля
         int m - количество мин на поле
         """
-        self.pole = [[Cell() for i in range(n)] for j in range(n)]
-        mns = [[i%n, i//n] for i in rt(range(n*n),m)]
+        self.n = n
+        self.m = m
+        self.field = [[Cell() for _ in range(n)] for _ in range(n)]
+        self.mines = rt(range(n*n), m)
+        for mine in self.mines:
+            row = mine // n
+            col = mine % n
+            self.field[row][col].is_mine = True
+            for r in range(row-1, row+2):
+                for c in range(col-1, col+2):
+                    if 0 <= r < n and 0 <= c < n and not self.field[r][c].is_mine:
+                        self.field[r][c].around_mines += 1
